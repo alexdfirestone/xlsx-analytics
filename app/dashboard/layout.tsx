0cfd01e96dcef381ee3ae56afae96500455c9a7e
Alex Firestone <alexdfirestone@gmail.com>
2025-07-31 17:12:45 -0700

    clean

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -23,19 +23,7 @@ export default function DashboardLayout({
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              {/* <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/dashboard">
-                    Dashboard
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Analytics</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList> */}
-            </Breadcrumb>
+           
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
